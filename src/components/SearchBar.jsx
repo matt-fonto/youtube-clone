866ca72d488d6d34282aafbfb0837b645e2f9934
SoftEngMatthew <mateus.fonto@gmail.com>
2022-11-23 11:37:21 -0300

    Basic app structure done. Changing the navbar

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Paper, IconButton } from "@mui/material";
+import { SearchIcon } from "@mui/icons-material";
+
+const SearchBar = () => {
+  return <div>SearchBar</div>;
+};
+
+export default SearchBar;
